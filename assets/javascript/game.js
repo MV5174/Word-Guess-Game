f183a776b6cb05e3ecfca8edefcc9a9d148a3e64
Michael Vega <ichigo509@yahoo.com>
2019-06-24 17:11:10 -0400

    inital commit for hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//declare wins, guessesLeft, and array with words
+
+//make function that when the window starts the comp chooses a word from the array
+
+//make a function that reads the user's key pressed
+
+//make a function that places the user's keys pressed into an empty array to show what you guessed
+
+//make a function that reads if the user's key pressed matches a letter in the chosen words
+
+//make a function that updates the wins if the user gets the complete word
+
+//make a function that restarts the game if guessesLeft reaches 0
+
+//console log any extras
